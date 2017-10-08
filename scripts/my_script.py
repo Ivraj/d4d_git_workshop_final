099e4b964acb0be9aa289708cfb189ba00842c14
Ivraj Seerha <iseerha@college.harvard.edu>
2017-10-08 14:13:58 -0400

    adding my_script.py

---
+++ scripts/my_script.py
@@ -0,0 +1,18 @@
+#! /bin/usr/env python
+#-*- coding: utf-8 -*-
+"""[Description of the scripts here]
+"""
+
+from __future__ import print_function
+
+def my_cool_fun():
+    """Runs AI, ML, cloud scripts"""
+    # TODO: Add some AI, ML, cloud stuff here
+    print("Running super conplex AI, ML, cloud stuff")
+
+def main():
+    """Runs print functions"""
+    my_cool_fun()
+
+if "__name__" == "__main__":
+    main()
